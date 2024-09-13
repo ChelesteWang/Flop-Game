0e4b63dc17851682f29484ebe0019d739cd4c3b8
ChelesteWang <1581446178@qq.com>
2024-09-13 10:15:27 +0800

    fix: bug

--- src/script.js
+++ src/script.js
@@ -213,7 +213,6 @@ function selectRandomPunishment() {
 function clearPunishment() {
   document.getElementById("selected-punishment").textContent = "";
   punishmentsContainer.innerHTML = "";
-  initialPunishments = [];
   localStorage.removeItem("punishments");
 }
 
